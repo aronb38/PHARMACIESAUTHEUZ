50b7f8c62e96b0646fbdf1e8cd8fcb3fb74b7689
Aron <105177803+aronb38@users.noreply.github.com>
2023-11-28 16:02:21 +0100

    Démarrage git correct

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
